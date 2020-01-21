ee9091ecc82d5928308201b85100159c9b7a7135
dealer426 <burnssamuel01@gmail.com>
2020-01-21 18:44:44 -0500

    Added Game Menu and Execution Menu to Menu Class

--- Menu.cs
+++ Menu.cs
@@ -8,6 +8,8 @@ namespace VirtualPet
     {
         public string MenuName { get; set; }
 
+        readonly VirtualPet myCat = new VirtualPet();
+
         public string AppMenu()
         {
             Console.WriteLine("Virtual Pet Menu");
@@ -19,9 +21,36 @@ namespace VirtualPet
       
         }
 
-        public string GameMenu(string menuCHoice)
+        public string GameMenu()
         {
-            return menuCHoice;
+            Console.WriteLine("1. Add a Pet to shelter");
+            Console.WriteLine("2. View the Pets Name and Species");
+            Console.WriteLine("3. Feed your Pet.");
+            Console.Write("10. Exit this Game");
+            string gameMenuChoice = Console.ReadLine();
+            return gameMenuChoice;
+        }
+
+        public void ExecuteMenuChoice(string menuChoice)
+        {
+            switch (menuChoice)
+            {
+                case "1":
+                    myCat.IsInShelter = true;
+                    myCat.AddPetToShelter();
+                    myCat.SetPetName();
+                    myCat.SetPetSpecies();
+                    break;
+                case "2":
+                    myCat.ViewPetInfo();
+                    break;
+                case "3":
+                    Console.WriteLine(myCat.HungerLevel);
+                    myCat.FeedPet();
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -6,46 +6,20 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            //Create Virtual Pet
-            VirtualPet myCat = new VirtualPet();
-            Menu gameMenu = new Menu();
-
+            Menu myMenu = new Menu();
             string mainMenuChoice = "";
             string gameMenuChoice = "";
 
 
             while(mainMenuChoice != "2")
             {
-                mainMenuChoice = gameMenu.AppMenu();
+                mainMenuChoice = myMenu.AppMenu();
 
                 while (gameMenuChoice != "10")
                 {
-                    Console.WriteLine("1. Add a Pet to shelter");
-                    Console.WriteLine("2. View the Pets Name and Species");
-                    Console.WriteLine("3. Feed your Pet.");
-                    Console.Write("10. Exit this Game");
-                    gameMenuChoice = Console.ReadLine();
+                    gameMenuChoice = myMenu.GameMenu();
 
-                    switch (gameMenuChoice)
-                    {
-                        case "1":
-                            myCat.IsInShelter = true;
-                            myCat.AddPetToShelter();
-                            myCat.SetPetName();
-                            myCat.SetPetSpecies();
-                            break;
-                        case "2":
-                            myCat.ViewPetInfo();
-                            break;
-                        case "3":
-                            Console.WriteLine(myCat.HungerLevel);
-                            myCat.FeedPet();
-                            break;
-                        default:
-                            break;
-                    }
-
-
+                    myMenu.ExecuteMenuChoice(gameMenuChoice);
 
                 }
 
