e20855d4d16db3fe6528c577f9050891cf8d1340
dealer426 <burnssamuel01@gmail.com>
2020-01-21 18:21:26 -0500

    Initial Commit to Git Hub

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Virtual Pet
+            VirtualPet myCat = new VirtualPet();
+            string mainMenuChoice = "";
+            string gameMenuChoice = "";
+
+
+            while(mainMenuChoice != "2")
+            {
+                Console.WriteLine("Virtual Pet Menu");
+                Console.WriteLine("Welcome to Virtual Pet");
+                Console.WriteLine("1. Play the Game");
+                Console.Write("2. Exit the App");
+                mainMenuChoice = Console.ReadLine();
+
+                while (gameMenuChoice != "10")
+                {
+                    Console.WriteLine("1. Add a Pet to shelter");
+                    Console.WriteLine("2. View the Pets Name and Species");
+                    Console.WriteLine("3. Feed your Pet.");
+                    Console.Write("10. Exit this Game");
+                    gameMenuChoice = Console.ReadLine();
+
+                    switch (gameMenuChoice)
+                    {
+                        case "1":
+                            myCat.IsInShelter = true;
+                            myCat.AddPetToShelter();
+                            myCat.SetPetName();
+                            myCat.SetPetSpecies();
+                            break;
+                        case "2":
+                            myCat.ViewPetInfo();
+                            break;
+                        case "3":
+                            Console.WriteLine(myCat.HungerLevel);
+                            myCat.FeedPet();
+                            break;
+                        default:
+                            break;
+                    }
+
+
+
+                }
+
+            }
+           
+
+        }
+    }
+}
---
+++ VirtualPet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPet
+{
+    public class VirtualPet
+    {
+        // add properties here
+
+        public string Name { get; set; }
+        public int HungerLevel { get; set; } = 5;
+        public string Species { get; set; }
+        public bool IsInShelter { get; set; }
+
+
+        // add constructors here
+        public VirtualPet()
+        {
+        
+        }
+
+        public VirtualPet(string name, int hungerLevel, string species, bool isInShelter)
+        {
+            Name = name; 
+            HungerLevel = hungerLevel;
+            Species = species;
+            IsInShelter = isInShelter;
+        }
+
+        // add methods here
+        public void SetPetName()
+        {
+            Console.WriteLine("What is the Pet's name?");
+            string UsersPetsName = Console.ReadLine();
+            Name = UsersPetsName;
+
+        }
+
+        public void SetPetSpecies()
+        {
+            Console.WriteLine("What is your Pets species?");
+            string UserPetsSpecies = Console.ReadLine();
+            Species = UserPetsSpecies;
+        }
+
+
+        public void ViewPetInfo()
+        {
+            Console.WriteLine($"My Pets name is {Name}. Its Species is {Species}.");
+
+        }
+
+        public void AddPetToShelter()
+        {
+            IsInShelter = true;
+            Console.WriteLine((IsInShelter) ? "Your Pet is in the shelter." : "Something Went Wrong!");
+
+        }
+
+        public void RemovePetFromShelter()
+        {
+            IsInShelter = false;
+        }
+
+        public void FeedPet()
+        {
+            Console.WriteLine($"Current Hunger level decreases by 1 {--HungerLevel}"); 
+        }
+
+    }
+}
